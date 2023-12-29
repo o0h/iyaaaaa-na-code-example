6a9c2ce34cfda6e22ed0cf4ac2765a4d7eda915b
o0h <dev@o0h.in>
2023-12-30 03:38:38 +0900

    コメントの整理

--- Obento/src/BentoOrderManager.php
+++ Obento/src/BentoOrderManager.php
@@ -6,41 +6,64 @@ namespace O0h\Obento;
 
 class BentoOrderManager
 {
-    private $orders = [];
+    /** @var array<BentoOrder> */
+    private array $orders = [];
 
+    /**
+     * 注文を追加する.
+     *
+     * @param BentoOrder $order
+     */
     public function addOrder($order): void
     {
         $this->orders[] = $order;
     }
 
+    /**
+     * 複数の注文の金額を確定し、データを保存する.
+     *
+     * - 引換券を適用する
+     * - 注文データを保存する
+     *
+     * @param bool $usingVoucher 引換券を利用するか
+     */
     public function finalizeOrders(bool $usingVoucher = false): void
     {
         if ($usingVoucher) {
             $this->useVoucher();
         }
 
-        // 注文を保存する
         foreach ($this->orders as $order) {
             $order->registerOrder();
         }
     }
 
-    private function useVoucher()
+    /**
+     * 引換券の利用処理を行う.
+     *
+     * - 適用対象の注文を選定する
+     * - 適用対象であることをマークする
+     * - 適用できる注文が存在しなかった場合はエラーメッセージを出力する
+     */
+    private function useVoucher(): void
     {
         $toApplyVoucher = $this->getOrderToApplyVoucher();
         if (!$toApplyVoucher) {
             echo "引換券を利用する条件を満たす注文がありません。\n";
+
             return;
         }
 
         $toApplyVoucher->applyVoucher();
     }
 
-    private function getOrderToApplyVoucher()
+    /**
+     * 適用対象の注文を選定する.
+     */
+    private function getOrderToApplyVoucher(): ?BentoOrder
     {
         $candidate = null;
 
-        // 引換券での引き換え対象のうち、最高価格の注文を選別する
         foreach ($this->orders as $order) {
             if (!$order->isVoucherApplicable()) {
                 continue;
