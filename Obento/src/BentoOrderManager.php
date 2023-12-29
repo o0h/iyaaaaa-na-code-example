927d7739ba9d739cf448b8190b01935beae66cb5
o0h <dev@o0h.in>
2023-12-30 02:16:32 +0900

    それぞれの手続きに「意図」の読みがなを振る

--- Obento/src/BentoOrderManager.php
+++ Obento/src/BentoOrderManager.php
@@ -15,23 +15,29 @@ class BentoOrderManager
 
     public function finalizeOrders(bool $usingVoucher = false): void
     {
+        // 引換券を利用したい場合は
         if ($usingVoucher) {
+            // 引換券を適用する対象となる注文を選別して
             $maxPriceOrder = null;
             $maxPrice = 0;
 
             foreach ($this->orders as $order) {
+                // 引換券での引き換え対象のうち、最高価格の注文を選別する
                 if ($order->isVoucherApplicable() && $order->getBasePrice() > $maxPrice) {
                     $maxPriceOrder = $order;
                     $maxPrice = $order->getBasePrice();
                 }
             }
 
+            // 対象があれば、引換券を適用する
             if ($maxPriceOrder) {
                 $maxPriceOrder->applyVoucher();
-            } else {
+            } else { // 対象がなければエラーメッセージを出力する
                 echo "引換券を利用する条件を満たす注文がありません。\n";
             }
         }
+
+        // 注文を保存する
         foreach ($this->orders as $order) {
             $order->registerOrder();
         }
