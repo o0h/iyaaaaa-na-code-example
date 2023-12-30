ae30e074dee6ceb3d6890265a03fc20509963dfd
o0h <dev@o0h.in>
2023-12-30 20:15:38 +0900

    注文受け付け可否を判定するクラスを新設

---
+++ Obento/src/BentoOrderValidator.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace O0h\Obento;
+
+class BentoOrderValidator
+{
+    public function __construct(private $productId, private int $quantity, private bool $isPreOrder, private BentoDB $db) {}
+
+    public function isOrderAcceptable()
+    {
+        if ($this->db->isReservationOnly($this->productId) && !$this->isPreOrder) {
+            return false;
+        }
+        $stockData = $this->db->getStock($this->productId);
+        $currentStock = $stockData['stock'] ?? 0;
+        $reservedStock = $stockData['reserved_stock'] ?? 0;
+
+        return $currentStock - $reservedStock >= $this->quantity;
+    }
+}
